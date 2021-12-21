3c071e0b45ce930554a13a3eb95ef9e7ea092812
CRW <chris.wilhite@datastax.com>
2021-12-21 08:44:46 -0500

    add cassandra models

--- week4-api-microservices/python/api/models.py
+++ week4-api-microservices/python/api/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+import uuid
+from datetime import datetime
+from cassandra.cqlengine import columns
+from django_cassandra_engine.models import DjangoCassandraModel
 
-# Create your models here.
+
+class TodoItem(DjangoCassandraModel):
+    id = columns.UUID(primary_key=True, default=uuid.uuid4)
+    text = columns.Text(required=True)
+    order = columns.Integer(null=True, blank=True)
+    completed = columns.Boolean(null=True, blank=True, default=False)
+    created_at = columns.DateTime(default=datetime.now)
--- week4-api-microservices/python/todos/settings.py
+++ week4-api-microservices/python/todos/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from cassandra.auth import PlainTextAuthProvider
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +77,16 @@ WSGI_APPLICATION = 'todos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_cassandra_engine',
+        'NAME': config('ASTRA_DB_KEYSPACE'),
+        'OPTIONS': {
+            'connection': {
+                'auth_provider': PlainTextAuthProvider('token', config('ASTRA_DB_APPLICATION_TOKEN')),
+                'cloud': {
+                    'secure_connect_bundle': config('ASTRA_DB_BUNDLE_PATH')
+                }
+            }
+        }
     }
 }
 
