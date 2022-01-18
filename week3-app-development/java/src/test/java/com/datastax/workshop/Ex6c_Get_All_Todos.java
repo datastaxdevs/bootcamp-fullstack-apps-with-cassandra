e10a9d9b5522c91cd0ce27ec29b514a7d2f78c69
Rags <rags@acm.org>
2022-01-18 16:29:30 +0000

    Simple object mapper for Get all

--- week3-app-development/java/src/test/java/com/datastax/workshop/Ex6c_Get_All_Todos.java
+++ week3-app-development/java/src/test/java/com/datastax/workshop/Ex6c_Get_All_Todos.java
@@ -12,6 +12,10 @@ import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @RunWith(JUnitPlatform.class)
 @TestMethodOrder(OrderAnnotation.class)
 public class Ex6c_Get_All_Todos implements DBConnection {
@@ -20,7 +24,7 @@ public class Ex6c_Get_All_Todos implements DBConnection {
   @Order(1)
   public void should_get_todos_1() {
     System.out.println("[should_get_todos_1] ========================================");
-    System.out.println("[should_get_todos_1] Looking for John Tasks");
+    System.out.println("[should_get_todos_1] Looking for John's Tasks");
     try (CqlSession cqlSession = TestUtils.createCqlSession()) {
       ResultSet rs = cqlSession.execute(
         "SELECT * FROM todoitems WHERE user_id = 'john';"
@@ -53,5 +57,46 @@ public class Ex6c_Get_All_Todos implements DBConnection {
     }
     System.out.println("[should_get_todos_2] [OK]");
     System.out.println("[should_get_todos_2] ========================================\n");
+  }
+  @Test
+  @Order(3)
+  public void should_get_todos_3() {
+    class TodoItem {
+      public String user_id;
+      public UUID item_id;
+      public String title;
+      public Boolean completed;
+
+      TodoItem(String u, UUID i, String t, Boolean c) {
+        this.user_id = u;
+        this.item_id = i;
+        this.title = t;
+        this.completed = c;
+      }
+    }
+    System.out.println("[should_get_todos_3 with simple object mapper]=================");
+    System.out.println("[should_get_todos_3] Looking for 'createTask' Tasks");
+    try (CqlSession cqlSession = TestUtils.createCqlSession()) {
+      ResultSet rs = cqlSession.execute(
+        "SELECT * FROM todoitems WHERE user_id='createTask';"
+      );
+
+      List<TodoItem> result = new ArrayList<>();
+
+      // Simple Object Mapper
+
+      rs.forEach(item -> result.add(
+            new TodoItem(item.getString("user_id"),
+                         item.getUuid("item_id"),
+                         item.getString("title"),
+                         item.getBoolean("completed"))
+        ));
+
+      for (TodoItem item: result) {
+        System.out.println("Todoitem title = " + item.title);
+      }
+    }
+    System.out.println("[should_get_todos_3 with simple object mapper] [OK]");
+    System.out.println("[should_get_todos_3] ========================================\n");
   }
 }
