3150bef0f0612ebc59281a62dc2b7832b7110117
CRW <chris.wilhite@datastax.com>
2022-01-26 10:28:17 -0500

    update keyspace

--- week4-api-microservices/python/todos.py
+++ week4-api-microservices/python/todos.py
@@ -5,7 +5,7 @@ from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 from cassandra.cqlengine.management import sync_table
 from cassandra.cqlengine import connection
-from connection import session
+from connection import session, KEYSPACE
 
 load_dotenv()
 session.default_timeout = 60
@@ -13,7 +13,7 @@ connection.register_connection("session", session=session, default=True)
 
 
 class Todos(Model):
-    __keyspace__ = os.environ.get("ASTRA_DB_KEYSPACE")
+    __keyspace__ = KEYSPACE
     __table_name__ = "todoitems"
     user_id = columns.Text(primary_key=True, required=True)
     item_id = columns.TimeUUID(
