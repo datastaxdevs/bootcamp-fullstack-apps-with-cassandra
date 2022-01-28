4efbe8b234a67d1478d5d49e14585dacf67b1c54
Uko Uche <ukouche80@gmail.com>
2022-01-28 10:51:22 +0000

    commit changes

---
+++ week3-app-development/python/ExTest_connect_to_Todo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from connection import session
+
+print('========================================')
+print('Start exercise')
+try:
+    output = session.execute("SELECT * FROM todoitems WHERE user_id = 'john';")
+
+    lslist = [str(row).split(' , ') for row in output if 'False' in str(row)]
+    #print(lslist)
+    for item in lslist[0]:
+        print(type(item))
+    """ for row in output:
+        #print(str(row).split(' , '))
+        if 'False' in str(row):
+            print(str(row).split(' , ')) """                 
+
+except Exception as e:
+    print(e)
+    print('Failure')
+else:
+    print('Success')
+print('========================================')
